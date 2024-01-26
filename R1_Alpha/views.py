036566ac3bfe626a216e200139b243bd71b02f4d
ranjith7333 <157765288+ranjith7333@users.noreply.github.com>
2024-01-26 05:45:11 +0530

    Code commit

---
+++ R1_Alpha/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+# Pages 
+
+def home(request):
+    return render(request,"home.html")
+
+def clocking(request):
+    EMPID=request.POST ['EMP ID']   
+    WONO=request.POST ['WO#']  
+    CATAGORY=request.POST ['CATAGORY']  
+    HOURS=request.POST ['HOURS']  
+    DESCRIPTION=request.POST ['DESCRIPTION']  
+    return render(request,"checksheet.html",{'EMPID':EMPID,'WONO':WONO,'CATAGORY':CATAGORY,'HOURS':HOURS,'DESCRIPTION':DESCRIPTION})
+
+def timesheet(request):
+    return render(request,"timesheet.html")
+    return HttpResponse(msg)
+
+def workorder(request):
+    msg="<h1>Manage you workorders here<h1>"
+    return HttpResponse(msg)
+
+def user(request):
+    msg="<h1>Manage your account here<h1>"
+    return HttpResponse(msg)
+
+def contact(request):
+    msg="<h1>Contact Admin for more Details<h1>"
+    return HttpResponse(msg)
