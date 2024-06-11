d2631277e7ba5e4ea4c8677ad5980bbbf875d835
ranjith7333 <157765288+ranjith7333@users.noreply.github.com>
2024-06-11 19:54:32 +0530

    Update views.py

--- R1_Alpha/views.py
+++ R1_Alpha/views.py
@@ -7,7 +7,10 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,"home.html")
-
+    
+def index(request):
+    return render(request,"index.html")
+    
 def clocking(request):
     EMPID=request.POST ['EMP ID']   
     WONO=request.POST ['WO#']  
